e035a482b8d283445107991a112495c91d9583de
Mahmoud Garwallane <garwallanemahmoud@gmail.com>
2023-06-25 18:11:10 +0100

    fix: docker compose hostname

--- server/nodejs_server/src/index.js
+++ server/nodejs_server/src/index.js
@@ -9,17 +9,22 @@ const {
   Hbar,
   TransferTransaction,
 } = require("@hashgraph/sdk");
-require("dotenv").config();
 const { InfluxDB, Point } = require("@influxdata/influxdb-client");
 const protobuf = require("protobufjs");
-const { token } = require("./env.js");
+const {token,privateKey,accountId} = require("./env.js");
 
-const myAccountId = process.env.MY_ACCOUNT_ID;
-const myPrivateKey = process.env.MY_PRIVATE_KEY;
+// const myAccountId = process.env.MY_ACCOUNT_ID;
+// const myPrivateKey = process.env.MY_PRIVATE_KEY;
+const myAccountId = accountId;
+const myPrivateKey = privateKey;
 const topicId = "0.0.14978148";
-const url = "http://localhost:8086";
+const url = "http://influxdb:8086";
 const org = "TCi";
 const bucket = "instaplant";
+
+if (!myAccountId || !myPrivateKey) {
+  throw new Error("Environment variables MY_ACCOUNT_ID and MY_PRIVATE_KEY must be present");
+}
 
 // Create your Hedera Testnet client
 const client = Client.forTestnet();
